88f1ed3ba85b9e5eb3ba4c52eaa1328d39a7241f
sachinpalsp917 <sachinpalsp917@gmail.com>
2026-05-16 11:53:59 +0530

    Added State management using zustand

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,10 @@ import { Tooltip } from "react-tooltip";
 import { useGSAP } from "@gsap/react";
 import { dockApps } from "#constants";
 import gsap from "gsap";
+import useWindowStore from "#store/window";
 
 const Dock = () => {
+  const { openWindow, closeWindow, windows } = useWindowStore();
   const dockRef = useRef(null);
 
   useGSAP(() => {
@@ -57,7 +59,20 @@ const Dock = () => {
   }, []);
 
   const toogleApp = (app) => {
-    // TODO Implement Open Window Logic
+    if (!app.canOpen) return;
+
+    const window = windows[app.id];
+
+    if (!window) {
+      console.log(`Window not found for app: ${app.id}`);
+      return;
+    }
+
+    if (window.isOpen) {
+      closeWindow(app.id);
+    } else {
+      openWindow(app.id);
+    }
   };
 
   return (
