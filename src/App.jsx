9a7fd133e6fee9cac07535fcea329433d65c0616
sachinpalsp917 <sachinpalsp917@gmail.com>
2026-05-18 13:00:02 +0530

    added the text and image window

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Dock, Navbar, Welcome } from "#components";
-import { Finder, Resume, Safari, Terminal } from "#windows";
+import { Finder, Image, Resume, Safari, Terminal, Text } from "#windows";
 import gsap from "gsap";
 
 import { Draggable } from "gsap/Draggable";
@@ -16,6 +16,8 @@ const App = () => {
       <Safari />
       <Resume />
       <Finder />
+      <Text />
+      <Image />
     </main>
   );
 };
