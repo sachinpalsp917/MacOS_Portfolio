6d69649b108567bf8daec40823bb436d69a63272
sachinpalsp917 <sachinpalsp917@gmail.com>
2026-05-18 11:45:15 +0530

    added the resume window

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { navIcons, navLinks } from "#constants";
+import useWindowStore from "#store/window";
 import dayjs from "dayjs";
 
 const Navbar = () => {
+  const { openWindow } = useWindowStore();
+
   return (
     <nav>
       <div className="">
@@ -9,8 +12,8 @@ const Navbar = () => {
         <p className="font-bold">Sachin's Portfolio</p>
 
         <ul>
-          {navLinks.map(({ id, name }) => (
-            <li key={id}>
+          {navLinks.map(({ id, name, type }) => (
+            <li key={id} onClick={() => openWindow(type)}>
               <p>{name}</p>
             </li>
           ))}
